10bdf4d02f4de04482866e1c79b26c3c2f36088d
Brett Pontarelli <brett@paperyfrog.com>
2013-09-18 17:13:12 -0700

    Added version global variable and fixed paths on build

--- config.rb
+++ config.rb
@@ -3,3 +3,11 @@ page 'index.html', layout: false
 set :css_dir, 'stylesheets'
 set :js_dir, 'javascripts'
 set :images_dir, 'images'
+
+set :version, '0.2.2'
+
+configure :build do
+  activate :minify_css
+  activate :minify_javascript
+  set :http_prefix, '/jaff'
+end
